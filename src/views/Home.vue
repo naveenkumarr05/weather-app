bf76bf63165292b7aa5222edce0a3a83813c6d47
Naveen kumar <naveenkumar8546@gmail.com>
2022-02-13 21:09:55 +0400

    added custom css for search field and buttons

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <SearchField/>
+    <DayForecastView/>
+    <WeatherReport/>
+  </div>
+</template>
+
+<script>
+import WeatherReport from '@/components/WeatherReport.vue'
+import SearchField from '@/components/SearchField.vue'
+import DayForecastView from '@/components/DayForecastView.vue'
+
+export default {
+  name: 'home',
+  components: {
+    WeatherReport,
+    SearchField,
+    DayForecastView
+  },
+}
+</script>
+<style>
+body {
+  background: #fff;
+  margin: 20px;
+}
+</style>
